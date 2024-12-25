c990b7b855004319c9ec28b9cf3e580ac98a388b
minh-quang-pham-le <mkw.dsa@gmail.com>
2024-12-26 06:12:48 +0700

    Modify CreateReceipt

--- Controller/CreateReceipt.java
+++ Controller/CreateReceipt.java
@@ -33,8 +33,8 @@ public class CreateReceipt implements Option {
             Product product = database.findProductById(productID);
             
             if (product == null) {
-                System.out.println("Invalid Product ID. Skipping...");
-                continue;
+                System.out.println("Invalid product ID. Aborting receipt creation.");
+                return;
             }
             
             System.out.println("Enter quantity to purchase:");
