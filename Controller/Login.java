8bbdc399d1ac75459593264a775d1a366c0e9e96
minh-quang-pham-le <mkw.dsa@gmail.com>
2024-12-26 06:58:20 +0700

    Modify Login

--- Controller/Login.java
+++ Controller/Login.java
@@ -9,24 +9,32 @@ public class Login implements Option {
     private Employee employee;
     private boolean loggedIn;
     
-    public void operate(Employee user, Scanner s, Database database,History history) {
-		System.out.println("Welcome to HUST book store management system");
-		System.out.println("Enter your email:");
-		String email = s.next();
-		System.out.println("Enter your password:");
-		String password = s.next();
-		Controller.Login login = new Controller.Login(email, password, database);
+    public void operate(Employee user, Scanner s, Database database, History history) {
+        while (true) {
+            System.out.println("Welcome to HUST book store management system");
+            System.out.println("Enter your email:");
+            String email = s.next();
+            System.out.println("Enter your password:");
+            String password = s.next();
+            Controller.Login login = new Controller.Login(email, password, database);
 
-		if (login.isLoggedIn()) {
-			Employee employee = login.getUser();
-			System.out.println("Welcome "+ employee.getName());
-			employee.showList(s, database,history);
-		} 
-        else {
-			System.out.println("Wrong email or password!");
-			System.out.println("Try agin later");
-		}
-	}
+            if (login.isLoggedIn()) {
+                Employee employee = login.getUser();
+                System.out.println("Welcome " + employee.getName());
+                employee.showList(s, database, history);
+                break;
+            } else {
+                System.out.println("Wrong email or password!");
+                System.out.println("1. Try again");
+                System.out.println("2. Exit system");
+                int choice = s.nextInt();
+                if (choice == 2) {
+                    System.out.println("Exiting the system. Goodbye!");
+                    System.exit(0);
+                }
+            }
+        }
+    }
 
     public Login(String email, String password, Database database) {
         this.loggedIn = false;
