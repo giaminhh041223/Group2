adfd1b466f480a159163cdd8705f1bd8d4fcb38a
minh-quang-pham-le <mkw.dsa@gmail.com>
2024-12-26 06:27:36 +0700

    Add function for storekeeper

--- Model/Storekeeper.java
+++ Model/Storekeeper.java
@@ -1,8 +1,12 @@
 package Model;
 
+import Controller.*;
+
 public class Storekeeper extends Employee {
+
 	public Storekeeper(String ID, String name, String email,String password, double salary) {
 		super(ID,name,email,password,salary);
+		generateOptions();
 	}
 	public Storekeeper(){
 		generateOptions();
@@ -10,6 +14,9 @@ public class Storekeeper extends Employee {
 	
 	public void generateOptions() {
         this.options = new Option[] {
+			new UpdateProduct(),
+			new ReadAllProducts(),
+			new ReadAllProductsByType(),
         };
 	}
 	
