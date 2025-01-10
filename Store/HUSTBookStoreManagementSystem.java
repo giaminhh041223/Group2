78efced49b8fac710bbe976e8bc309834f181979
Minh Kwang <mkw.dsa@gmail.com>
2025-01-10 14:18:24 +0700

    Update

---
+++ Store/HUSTBookStoreManagementSystem.java
@@ -0,0 +1,19 @@
+package Store;
+
+import java.util.Scanner;
+import Controller.*;
+import History.History;
+import Model.*;
+
+public class HUSTBookStoreManagementSystem {
+    public static void main(String[] args) {
+        Database database = new Database();
+        FinancialSystem financialSystem = new FinancialSystem(1000000000.0);
+        History history = new History();
+
+        // Load employees and products
+        database.loadData("employees.csv", "products.csv", database, history);
+
+        new Login().operate(null, new Scanner(System.in), database, financialSystem, history);
+    }
+}
